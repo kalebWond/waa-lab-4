4bedf8a3d071663b20ce04610b825411a9427068
Kaleb Wondwossen <kaleb.wond@gmail.com>
2023-04-07 16:14:14 -0500

    finalize

---
+++ src/main/java/com/example/Lab4/domain/User.java
@@ -0,0 +1,24 @@
+package com.example.Lab4.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    String name;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    List<Post> posts;
+}
---
+++ src/main/java/com/example/Lab4/domain/dto/UserDto.java
@@ -0,0 +1,16 @@
+package com.example.Lab4.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+    long id;
+    String name;
+    List<PostDto> posts;
+}
